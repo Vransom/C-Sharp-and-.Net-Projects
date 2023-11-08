f81565fbe7c975aa9dc7add8e9e640b884efbd8c
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-08 10:51:46 -0800

    Add files via upload

---
+++ StringsAndIntegersSubmission/StringsAndIntegersSubmission/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndIntegersSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> integers = new List<int> { 2, 4, 6, 8, 10 };
+                Console.WriteLine("Please enter a number to divide a list of numbers by:");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int integer in integers)
+                {
+                    Console.WriteLine(Convert.ToString(integer / input));
+                }
+            }
+ 
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number, do not type out the word format of the number");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please dont divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+ }
+
