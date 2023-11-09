31e55d96a4bdf48c1678c4586432b1cafa703835
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-09 14:29:17 -0800

    Add files via upload

---
+++ MainMethodSubmission/MainMethodSubmission/MathOperator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    public class MathOperator
+    {
+        public int Addition(int input)
+        {
+            return input + 100;
+        }
+
+        public int Decimal(decimal dec)
+        {
+            return Convert.ToInt32(dec) + 200;
+        }
+
+        public int String(string str)
+        {
+            return Convert.ToInt32(str) + 100;
+        }
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperator output = new MathOperator();
+
+            Console.WriteLine("Please enter an integer to perform a math operation on that integer:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(input + " plus 100 is " + output.Addition(input));
+
+            Console.WriteLine("\n Now please enter a decimal number to add:");
+            decimal dec = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(dec + " (rounded to the nearest integer) plus 200 is " + output.Decimal(dec));
+
+            Console.WriteLine("\n Now please enter an integer in word format:");
+            string str = Console.ReadLine();
+            Console.WriteLine(str + "(reformatted so the computer can understand it) plus 100 is " + output.String(str));
+
+            Console.ReadLine();
+        }
+    }
+}
