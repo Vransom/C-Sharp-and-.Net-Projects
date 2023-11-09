e320040dbee9174e7bc70d4f3c1584179a911ae0
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-09 15:19:48 -0800

    Add files via upload

---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            operation output = new operation();
+
+            Console.WriteLine("Please enter one number in integer form:");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("If you wish, please enter another number in integer form:");
+            int input2;
+            int result;
+            if (int.TryParse(Console.ReadLine(), out input2))
+            {
+                result = output.twoNumbers(input1, input2);
+                Console.WriteLine("We have added " + input1 + " and " + input2 + " to equal " + result);
+            }
+            else
+            {
+                result = output.oneNumber(input1);
+                Console.WriteLine("We have added 100 to " + input1 + " to get " + result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodSubmission/MethodSubmission/operation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    public class operation
+    {
+        public int twoNumbers(int input1, int input2)
+        {
+            return input1 + input2;
+        }
+
+        public int oneNumber(int input1, int? input2 = null)
+        {
+            return input1 + 100;
+        }
+    }
+}
