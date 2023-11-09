239873652fc61a20710ef45b645b558fc6d0925d
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-09 10:48:46 -0800

    Add files via upload

---
+++ CallingMethodsSubmission/CallingMethodsSubmission/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    public class MathOperations
+    {
+        public static int Addition(int input)
+        {
+            return input + 100;
+        }
+
+        public static int Subtraction(int input)
+        {
+            return input - 100;
+        }
+
+        public static int Multiplication(int input)
+        {
+            return input * 100;
+        }
+
+    }
+}
---
+++ CallingMethodsSubmission/CallingMethodsSubmission/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer to perform three predetermined math operations on that integer:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int output = MathOperations.Addition(input);
+            Console.WriteLine(input + " plus 100 is " + output);
+            int output2 = MathOperations.Subtraction(input);
+            Console.WriteLine(input + " minus 100 is " + output2);
+            int output3 = MathOperations.Multiplication(input);
+            Console.WriteLine(input + " times 100 is " + output3);
+            Console.ReadLine();
+        }
+    }
+}
