37703b45d9a53fb4f3b63626493b12825cc935e6
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-10 12:12:53 -0800

    Add files via upload

---
+++ ClassSubmission/ClassSubmission/staticClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    public class staticClass
+    {
+        public static int Addition(int input4)
+        {
+            return input4 + 400;
+        }
+    }
+}
