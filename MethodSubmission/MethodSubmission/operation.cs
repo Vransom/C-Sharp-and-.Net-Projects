6807162d3f40d0ecf8f98fc0b1c64a868157a8ec
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-10 10:36:55 -0800

    Add files via upload

---
+++ MethodSubmission/MethodSubmission/operation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    public class operation
+    {
+        public int twoNumbers(int input1, int input2=0)
+        {
+            return input1 + input2;
+        }
+    }
+}
