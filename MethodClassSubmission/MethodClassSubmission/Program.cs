4276b6957f05fd9ca6df607c0986b099c5fe31e8
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-10 11:12:12 -0800

    Add files via upload

---
+++ MethodClassSubmission/MethodClassSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathoperator output = new mathoperator(); //instantiate
+            Console.WriteLine("Please enter one integer:");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second integer:");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            output.voidOperation(3, 7); //passing in two numbers
+
+            output.voidOperation(integer1: input1, integer2: input2); //passing in parameters by name
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassSubmission/MethodClassSubmission/mathoperator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    public class mathoperator
+    {
+        public void voidOperation(int integer1, int integer2)
+        {
+            int addition = integer1 + 100;
+            Console.WriteLine(integer2);
+        }
+
+    }
+}
