a7c7fb55a0131a2321007453aa33ec35a176955c
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-14 14:27:19 -0800

    Add files via upload

---
+++ OperatorsSubmission/OperatorsSubmission/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int ID { get; set; }
+
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            if (employee1.ID == employee2.ID)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.ID != employee2.ID)
+                return true;
+            else
+                return false;
+        }
+
+
+    }
+}
---
+++ OperatorsSubmission/OperatorsSubmission/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Victoria";
+            employee1.LastName = "Ransom";
+            employee1.ID = 1;
+            employees.Add(employee1);
+
+            Employee employee2 = new Employee();
+            employee1.FirstName = "John";
+            employee1.LastName = "Smith";
+            employee1.ID = 2;
+            employees.Add(employee2);
+
+            Console.WriteLine(employee1 == employee2);
+            Console.ReadLine();
+        }
+    }
+}
