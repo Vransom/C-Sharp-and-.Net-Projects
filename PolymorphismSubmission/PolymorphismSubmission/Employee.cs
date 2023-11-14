1d45c5173787e1259ee76ac476d6dd1a864433bc
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-14 13:40:06 -0800

    Add files via upload

---
+++ PolymorphismSubmission/PolymorphismSubmission/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSubmission
+{
+    public class Employee : Person, IQuittable
+    {
+        public void Quit()
+        {
+        Console.WriteLine("Quit");
+        }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ PolymorphismSubmission/PolymorphismSubmission/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+
+            IQuittable quit = new Employee();
+            quit.Quit();
+
+            Console.ReadLine();
+
+        }
+    }
+}
