ddfe8053533bb730e2d2df3a5ea03989bc9ad44d
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-14 16:00:39 -0800

    Add files via upload

---
+++ ParsingEnumsSubmission/ParsingEnumsSubmission/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsSubmission
+{
+    class Program
+    {
+        public enum daysoftheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the current day of the week:");
+            string input = Console.ReadLine();
+
+            try
+            {
+
+                Console.WriteLine(Enum.Parse(typeof(daysoftheWeek), input));
+
+            }
+
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
