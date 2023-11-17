21a7a8b99a000015f822ddd75fb50b46cab0a419
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-16 17:19:55 -0800

    Add files via upload

---
+++ DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter a number in integer format:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            DateTime hoursLater = DateTime.Now.AddHours(x);
+            Console.WriteLine("In " + x + " hours it will be " + hoursLater);
+            Console.ReadLine();
+
+        }
+    }
+}
