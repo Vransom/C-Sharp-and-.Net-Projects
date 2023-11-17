f8d58cb679449e8d57b48da32ea03d16b789e484
Victoria Ransom <140551153+Vransom@users.noreply.github.com>
2023-11-17 11:42:18 -0800

    Add files via upload

---
+++ Constructor Submission/Constructor Submission/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Submission
+{
+    public class Person
+    {
+        public Person(string firstname) : this(firstname, "Smith")
+        {
+        }
+
+        public Person (string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Constructor Submission/Constructor Submission/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Victoria");
+            const string Hotel = "Hilton";
+            Console.WriteLine("Welcome to the {0} Hotel, {1} {2}", Hotel, person.FirstName, person.LastName);
+            Console.ReadLine();
+        }
+    }
+}
